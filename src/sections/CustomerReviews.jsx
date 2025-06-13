2f90cdba89d7f07cc37b3920ad5b996e9a0cf0b7
benwales <benwales@bigpond.com>
2025-06-13 14:18:09 +1000

    Fix typos in customer reviews section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -5,7 +5,7 @@ const CustomerReviews = () => {
   return (
     <section className="max-container">
       <h3 className="font-palanquin text-center text-4xl font-bold">
-        Whare Our <span className="text-coral-red">Customers </span>Say?
+        What Our <span className="text-coral-red">Customers </span>Say?
       </h3>
       <p className="info-text m-auto mt-4 max-w-lg text-center">
         Hear genuine stories from our satisfied customers about their
